971cefb10f080ef44b05e81a5190fcadb929f5ce
Augusto Pinto <augustopinto@hotmail.es>
2021-02-26 20:06:29 -0300

    First Project creation

---
+++ app/src/main/java/com/apinto/tvseriesapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.apinto.tvseriesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
